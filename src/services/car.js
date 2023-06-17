1fc960be1fd237d970576a0dccfe04b8eec415be
ShahzodZiyodullayev <sshahzod5@gmail.com>
2023-06-17 10:57:27 +0500

    ++

--- src/services/car.js
+++ src/services/car.js
@@ -2,11 +2,11 @@ import axios from "./API";
 
 const CarDetailService = {
   async getCarDetail(route) {
+    const accessToken = JSON.parse(localStorage.getItem("accessToken"));
     const data = await axios.carDetailBaseURL
       .get(route, {
         headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlZGE4ZjRiLTY1NDYtNGRhZi1iZTY1LTc0NTBjNzljNWQ3MyIsImlhdCI6MTY4NjkyMDc1MCwiZXhwIjoxNjg3MDA3MTUwfQ.058082W1zCc05Y8tOze0cot2KFkXPOuf0lTjs0-vVNY",
+          Authorization: `Bearer ${accessToken}`,
         },
       })
       .then((a) => a?.data)
